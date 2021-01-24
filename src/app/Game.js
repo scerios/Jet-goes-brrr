56251a43c10a8ff0c8002ad2046fda45524a873c
Takacs Robert <89.t.robert@gmail.com>
2021-01-24 16:26:18 +0100

    feat(Game.js): added addSpritesToLoader()

--- src/app/Game.js
+++ src/app/Game.js
@@ -25,4 +25,25 @@ export class Game {
             this.app.loader.add(name, asset);
         }
     }
+
+    addSpritesToLoader() {
+        const modelSize = 100;
+        const halfPositionOnY = this.app.renderer.height / 2 - modelSize / 2;
+
+        this.app.loader.load((loader, resources) => {
+            this.sprites.playerJet = new PIXI.Sprite(resources.playerJet.texture);
+            this.sprites.playerJet.width = modelSize;
+            this.sprites.playerJet.height = modelSize;
+            this.sprites.playerJet.x = 0;
+            this.sprites.playerJet.y = halfPositionOnY;
+            this.app.stage.addChild(this.sprites.playerJet);
+
+            this.sprites.enemyJet = new PIXI.Sprite(resources.enemyJet.texture);
+            this.sprites.enemyJet.width = modelSize;
+            this.sprites.enemyJet.height = modelSize;
+            this.sprites.enemyJet.x = this.app.renderer.width - modelSize;
+            this.sprites.enemyJet.y = halfPositionOnY;
+            this.app.stage.addChild(this.sprites.enemyJet);
+        });
+    }
 }
