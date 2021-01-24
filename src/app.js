616d00d8b6b267c523aa2256230316a74095614f
Takacs Robert <89.t.robert@gmail.com>
2021-01-24 16:25:52 +0100

    feat(Game.js): added addAssetsToLoader()

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 import './scss/main.scss';
 import $ from 'jquery';
 import { Game } from './app/Game';
+import playerJet from './assets/images/playerJet.png';
+import enemyJet from './assets/images/enemyJet.png';
+import missile from './assets/images/missile.png';
+import frontTrees from './assets/images/frontTrees.png';
+import middleCity from './assets/images/middleCity.png';
+import middleCityShadow from './assets/images/middleCityShadow.png';
+import middleBackground from './assets/images/middleBackground.png';
+import middleBackgroundShadow from './assets/images/middleBackgroundShadow.png';
+import farSun from './assets/images/farSun.png';
+import farBackground from './assets/images/farBackground.png';
+import explosion from './assets/images/explosion.gif';
 
 const gameButtons = $('.game-btn');
+const splashScreen = $('.splash-screen');
+const menuContainer = $('.menu');
+const gameContainer = $('.game');
+
+const gameplayElements = [playerJet, enemyJet, missile, explosion];
+const backgroundImages = [frontTrees, middleCity, middleCityShadow, middleBackground, middleBackgroundShadow, farSun, farBackground];
+let game;
 
 gameButtons.on('click', () => {
-    let game = new Game();
-    console.log(game.hello);
-
-    for (let gameButton of gameButtons) {
-        $(gameButton).prop('disabled', true);
+    if (game == undefined) {
+        game = new Game(gameplayElements, backgroundImages);
+    } else {
+        game.startGame();
     }
+
+    gameContainer.append(game.app.view);
+
+    splashScreen.fadeOut();
+    menuContainer.fadeOut();
+    gameContainer.fadeIn();
 });
--- src/app/Game.js
+++ src/app/Game.js
@@ -2,8 +2,27 @@ import * as PIXI from 'pixi.js';
 
 export class Game {
     app;
+    sprites;
+    isRunning = false;
 
-    constructor() {
+    constructor(gameplayElements, backgroundImages) {
         this.app = new PIXI.Application({ width: 800, height: 600 });
+        this.sprites = {};
+        this.setup(gameplayElements, backgroundImages);
+    }
+
+    setup(gameplayElements, backgroundImages) {
+        this.addAssetsToLoader(gameplayElements);
+
+        this.addAssetsToLoader(backgroundImages)
+
+        this.addSpritesToLoader();
+    }
+
+    addAssetsToLoader(assets) {
+        for (let asset of assets) {
+            let name = asset.match(/(?<=\/)(.*?)(?=\.)/g)[0];
+            this.app.loader.add(name, asset);
+        }
     }
 }
